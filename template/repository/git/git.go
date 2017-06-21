7c59d7554ea51f7be384ddd2b8a6fcf140b1e408
Otto Giron <ottog2486@gmail.com>
2017-06-20 22:51:34 -0600

    #9 defines base repository

--- template/repository/git/git.go
+++ template/repository/git/git.go
@@ -6,6 +6,12 @@ var _ *repository.Repository = (*repository.Repository)(nil)
 
 //Repository represents an implementation of a ironman Repository
 type Repository struct {
+	*repository.BaseRepository
+}
+
+//New returns a new instance of the git repository
+func New(baseRepository *repository.BaseRepository) repository.Repository {
+	return &Repository{baseRepository}
 }
 
 //Install installs a template from a git url
@@ -15,35 +21,5 @@ func (r *Repository) Install(location string) error {
 
 //Update updates a template from a git repository
 func (r *Repository) Update(name string) error {
-	panic("not implemented")
-}
-
-//Uninstall uninstalls a template from a git repository
-func (r *Repository) Uninstall(name string) error {
-	panic("not implemented")
-}
-
-//Find finds an already installed template in this repository
-func (r *Repository) Find(name string) error {
-	panic("not implemented")
-}
-
-//IsInstalled verifies if a template is installed
-func (r *Repository) IsInstalled(name string) bool {
-	panic("not implemented")
-}
-
-//Installed returns a list of installed templates
-func (r *Repository) Installed() ([]string, error) {
-	panic("not implemented")
-}
-
-//Link links a template anywhere in the OS as an installed template
-func (r *Repository) Link(templatePath string, templateName string) error {
-	panic("not implemented")
-}
-
-//Unlink a linked template
-func (r *Repository) Unlink(templateName string) error {
 	panic("not implemented")
 }
--- template/repository/repository.go
+++ template/repository/repository.go
@@ -11,3 +11,47 @@ type Repository interface {
 	Link(templatePath string, templateName string) error
 	Unlink(templateName string) error
 }
+
+//BaseRepository implements basic generic repository operations
+type BaseRepository struct {
+}
+
+//Install not implemented for base repository since it depends on specific provider
+func (b *BaseRepository) Install(templateLocator string) error {
+	panic("not implemented")
+}
+
+//Update not implemented for base repository since it depend on specific provider
+func (b *BaseRepository) Update(templateID string) error {
+	panic("not implemented")
+}
+
+//Uninstall uninstalls a template
+func (b *BaseRepository) Uninstall(templateID string) error {
+	panic("not implemented")
+}
+
+//Find finds a template in the repository
+func (b *BaseRepository) Find(templateID string) error {
+	panic("not implemented")
+}
+
+//IsInstalled verifies if template is installed
+func (b *BaseRepository) IsInstalled(templateID string) (bool, error) {
+	panic("not implemented")
+}
+
+//Installed returns a lists of installed templates
+func (b *BaseRepository) Installed() ([]string, error) {
+	panic("not implemented")
+}
+
+//Link links a template on a path to the repository
+func (b *BaseRepository) Link(templatePath string, templateName string) error {
+	panic("not implemented")
+}
+
+//Unlink unlinks a linked template
+func (b *BaseRepository) Unlink(templateName string) error {
+	panic("not implemented")
+}
