ad880db49ccb35d889923d70dc49824a293f645f
Otto Giron <ottog2486@gmail.com>
2017-12-22 17:26:20 -0600

    #9 repository update method

--- template/repository/git/git.go
+++ template/repository/git/git.go
@@ -23,24 +23,42 @@ func New(baseRepository *repository.BaseRepository) repository.Repository {
 
 //Install installs a template from a git url
 func (r *Repository) Install(location string) error {
-	templateID := path.Base(strings.TrimSuffix(location, ".git"))
-	templatePath := r.TemplatePath(templateID)
+	templatePath := r.templatePathFromLocation(location)
 	gitRepo, err := gogit.NewFilesystemRepository(templatePath)
 
 	if err != nil {
-		return errors.Wrapf(err, "Failed to clone template repository %s", location)
+		return errors.Wrapf(err, "Failed to get template repository %s", location)
 	}
 	err = gitRepo.Clone(&gogit.CloneOptions{
 		URL: location,
 	})
 
 	if err != nil {
-		return errors.Wrapf(err, "Failed to clone template repository %s", location)
+		return errors.Wrapf(err, "Failed to install template  %s", location)
 	}
 	return nil
 }
 
 //Update updates a template from a git repository
-func (r *Repository) Update(name string) error {
-	panic("not implemented")
+func (r *Repository) Update(location string) error {
+	templatePath := r.templatePathFromLocation(location)
+	gitRepo, err := gogit.NewFilesystemRepository(templatePath)
+
+	if err != nil {
+		return errors.Wrapf(err, "Failed to get template repository %s", location)
+	}
+
+	err = gitRepo.Pull(&gogit.PullOptions{})
+
+	if err != nil {
+		return errors.Wrapf(err, "Failed to update template  %s", location)
+	}
+
+	return nil
+}
+
+func (r *Repository) templatePathFromLocation(location string) string {
+	templateID := path.Base(strings.TrimSuffix(location, ".git"))
+	templatePath := r.TemplatePath(templateID)
+	return templatePath
 }
