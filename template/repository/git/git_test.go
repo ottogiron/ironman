8dc8ce38e32e6ef74390edca7826bac96dbbea8e
Otto Giron <ottog2486@gmail.com>
2017-12-22 01:16:58 -0600

    #9 add basic tests for git repository install

--- template/repository/git/git_test.go
+++ template/repository/git/git_test.go
@@ -1,54 +1,49 @@
 package git
 
 import (
-	"reflect"
+	"os"
 	"testing"
 
 	"github.com/ironman-project/ironman/template/repository"
 )
 
-func TestNew(t *testing.T) {
-	type args struct {
-		baseRepository *repository.BaseRepository
-	}
-	tests := []struct {
-		name string
-		args args
-		want repository.Repository
-	}{
-	// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := New(tt.args.baseRepository); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("New() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+func newTestGitRepository() repository.Repository {
+	baseRepository := repository.NewBaseRepository("testing")
+	return New(baseRepository)
 }
 
 func TestRepository_Install(t *testing.T) {
-	type fields struct {
-		BaseRepository *repository.BaseRepository
-	}
+
 	type args struct {
 		location string
 	}
 	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
+		name               string
+		args               args
+		expectedTemplateID string
+		wantErr            bool
 	}{
-	// TODO: Add test cases.
+		{
+			"Install template",
+			args{"https://github.com/ottogiron/wizard-hello-world.git"},
+			"wizard-hello-world",
+			false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			r := &Repository{
-				BaseRepository: tt.fields.BaseRepository,
-			}
+			r := newTestGitRepository()
+			defer func() {
+				_ = r.Uninstall(tt.expectedTemplateID)
+			}()
 			if err := r.Install(tt.args.location); (err != nil) != tt.wantErr {
 				t.Errorf("Repository.Install() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			expectedTemplatePath := r.TemplatePath(tt.expectedTemplateID)
+			if _, err := os.Stat(expectedTemplatePath); os.IsNotExist(err) {
+				t.Errorf("Repository.Install() template was not installed want path %v", expectedTemplatePath)
 			}
 		})
 	}
