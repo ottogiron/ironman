f3fbfa8970c3102c65229b0f0170e5556a320ae6
Otto Giron <ottog2486@gmail.com>
2018-03-27 12:25:28 -0600

    implement simple go template engine

--- template/template.go
+++ template/template.go
@@ -1,1 +1,38 @@
 package template
+
+import (
+	"io"
+	gtemplate "text/template"
+)
+
+var _ Engine = (*goEngine)(nil)
+
+//Engine represents a template engine
+type Engine interface {
+	Parse(text string) (Engine, error)
+	Execute(writer io.Writer, data interface{}) error
+}
+
+type goEngine struct {
+	template *gtemplate.Template
+}
+
+//NewGoEngine returns a new instance of a go template engine
+func NewGoEngine(name string) Engine {
+	template := gtemplate.New(name)
+	return &goEngine{template}
+}
+
+func (g *goEngine) Parse(text string) (Engine, error) {
+	var err error
+	g.template, err = g.template.Parse(text)
+	if err != nil {
+		return nil, err
+	}
+
+	return g, nil
+}
+
+func (g *goEngine) Execute(writer io.Writer, data interface{}) error {
+	return g.template.Execute(writer, data)
+}
