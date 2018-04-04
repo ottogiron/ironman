96068f8d3b3080f26241ede91b7c7d6683e8e561
Otto Giron <ottog2486@gmail.com>
2018-04-03 18:30:04 -0600

    add acceptace test for file generator

--- acceptance/features/generate.feature
+++ acceptance/features/generate.feature
@@ -10,6 +10,29 @@ Feature: Generate a template
     Then The generate process state should be success 
     And The generate output should contain "Running template generator app" and "done"
     And A file "foobar" under the generated path should contain "Foo is bar and Bar is foo"
+
+   Scenario: Generate a file template with correct ID
+    Given A template to generate a file installed with URL "https://github.com/ironman-project/template-example.git"
+    When Generate file runs with ID "template-example" generator ID "controller"  and fileName "mycontroller.go"  and flags "name=MyController"
+    Then The generate file process state should be success 
+    And The generate file output should contain "Running template generator controller" and "done"
+    And A file under the generated path should contain 
+"""
+package controller
+
+import "net/http"
+
+
+
+type  MyControllerController struct {
+
+}
+
+func (c *MyControllerController) Handle(w http.ResponseWriter, r *http.Request) {
+	//Handle request
+}
+
+"""
    
   Scenario: Generate a template with non existing ID
     When Generate with non existing ID runs with ID "template-example-dont-exists" generator ID "app"
--- acceptance/generate.go
+++ acceptance/generate.go
@@ -3,15 +3,18 @@ package acceptance
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/DATA-DOG/godog"
+	"github.com/DATA-DOG/godog/gherkin"
 	"github.com/ironman-project/ironman/testutils"
 	"github.com/rendon/testcli"
 )
 
 var generatedPath string
+var generateFilePath string
 
 func aTemplateToGenerateInstalledWithURL(URL string) error {
 
@@ -78,6 +81,52 @@ func theGenerateWithNonExistingIDOutputShouldCointain(expectedOutput string) err
 	return nil
 }
 
+func aTemplateToGenerateAFileInstalledWithURL(URL string) error {
+	testcli.Run(testutils.ExecutablePath(), "install", "--ironman-home="+ironmanTestDir, URL)
+	if !testcli.Success() {
+		return fmt.Errorf("Failed to install test template %s", URL)
+	}
+	return nil
+}
+
+func generateFileRunsWithIDGeneratorIDAndFileNameAndFlags(templateID, generatorID, fileName, flags string) error {
+	generateFilePath = filepath.Join(ironmanTestDir, "testfile", fileName)
+	_ = os.Mkdir(filepath.Dir(generateFilePath), os.ModePerm)
+	testcli.Run(testutils.ExecutablePath(), "generate", templateID+":"+generatorID, generateFilePath, "--ironman-home="+ironmanTestDir, "--set", flags)
+	return nil
+}
+
+func theGenerateFileProcessStateShouldBeSuccess() error {
+	if !testcli.Success() {
+		return fmt.Errorf("Generate command did not succeeded %s\n %s %s", testcli.Stdout(), testcli.Error(), testcli.Stderr())
+	}
+	return nil
+}
+
+func theGenerateFileOutputShouldContainAnd(out1, out2 string) error {
+	if !(testcli.StdoutContains(out1) && testcli.StdoutContains(out2)) {
+		return fmt.Errorf("output => %s", testcli.Stdout())
+	}
+	return nil
+}
+
+func aFileFromAFileGeneratorUnderTheGeneratedPathShouldContain(contents *gherkin.DocString) error {
+
+	if !testutils.FileExists(generateFilePath) {
+		return fmt.Errorf("Expected file don't exists %s", generateFilePath)
+	}
+	fileContent, err := ioutil.ReadFile(generateFilePath)
+
+	if err != nil {
+		return fmt.Errorf("Failed to read file contents %s", err)
+	}
+
+	if string(fileContent) != contents.Content {
+		return fmt.Errorf("File content \n%s\n want \n%s\n", fileContent, contents.Content)
+	}
+	return nil
+}
+
 //GenerateContext context for generate command
 func GenerateContext(s *godog.Suite) {
 	s.Step(`^A template to generate installed with URL "([^"]*)"$`, aTemplateToGenerateInstalledWithURL)
@@ -85,7 +134,14 @@ func GenerateContext(s *godog.Suite) {
 	s.Step(`^The generate process state should be success$`, theGenerateProcessStateShouldBeSuccess)
 	s.Step(`^The generate output should contain "([^"]*)" and "([^"]*)"$`, theGenerateOutputShouldContainAnd)
 	s.Step(`^A file "([^"]*)" under the generated path should contain "([^"]*)"$`, aFileUnderTheGeneratedPathShouldContain)
+
 	s.Step(`^Generate with non existing ID runs with ID "([^"]*)" generator ID "([^"]*)"$`, generateWithNonExistingIDRunsWithIDGeneratorID)
 	s.Step(`^The generate with non existing ID process state should be failure$`, theGenerateWithNonExistingIDProcessStateShouldBeFailure)
 	s.Step(`^The generate with non existing ID output should cointain "([^"]*)"$`, theGenerateWithNonExistingIDOutputShouldCointain)
+
+	s.Step(`^A template to generate a file installed with URL "([^"]*)"$`, aTemplateToGenerateAFileInstalledWithURL)
+	s.Step(`^Generate file runs with ID "([^"]*)" generator ID "([^"]*)"  and fileName "([^"]*)"  and flags "([^"]*)"$`, generateFileRunsWithIDGeneratorIDAndFileNameAndFlags)
+	s.Step(`^The generate file process state should be success$`, theGenerateFileProcessStateShouldBeSuccess)
+	s.Step(`^The generate file output should contain "([^"]*)" and "([^"]*)"$`, theGenerateFileOutputShouldContainAnd)
+	s.Step(`^A file under the generated path should contain$`, aFileFromAFileGeneratorUnderTheGeneratedPathShouldContain)
 }
